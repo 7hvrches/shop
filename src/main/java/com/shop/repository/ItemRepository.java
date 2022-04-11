3e09d0128228d31700f89215e1e18cd73027cabb
7hvrches <7hvrches@naver.com>
2022-04-11 16:25:44 +0900

    상품 관리 페이지 구현을 위한 ItemController, ItemRepository, ItemService 추가 수정

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item>, ItemRepositoryCustom {
 
     List<Item> findByItemNm(String itemNm);
 
