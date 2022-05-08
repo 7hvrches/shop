e7220321e40114357052cdad3e1fcfcc0f3321c2
7hvrches <7hvrches@naver.com>
2022-05-08 19:16:26 +0900

    주문 이력 페이지 관련 비즈니스 로직 추가 수정

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -9,4 +9,7 @@ public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
 
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
+    //구매 이력 페이지에서 주문 상품의 대표 이미지를 보여주기 위해
+    ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn);
+
 }
