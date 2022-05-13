87463caa12a3c0fceedea3fcab61c2a33db3bb94
7hvrches <7hvrches@naver.com>
2022-05-13 12:29:54 +0900

    주문 취소 로직 추가 및 테스트

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -55,4 +55,7 @@ public class Item extends BaseEntity {
         this.stockNumber = restStock;
     }
 
+    public void addStock(int stockNumber) {
+        this.stockNumber += stockNumber;
+    }
 }
